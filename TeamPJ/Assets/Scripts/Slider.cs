54ec662788c9b9e2bb2d3fa4a366c6dcb9d4ad59
SANGHALEE <sangha206@naver.com>
2017-11-08 11:57:12 +0900

    포스터확대, 씬전환
    포스터확대+씬전환

---
+++ TeamPJ/Assets/Scripts/Slider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//using UnityEngine.XR;
+
+public class Slider : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private float expandMin = -3.0f;
+    private float expandMax = 3.0f;
+
+    public void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void Update()
+    {
+        //Debug.Log(rectTransform.position.x);
+        if (rectTransform.position.x < expandMax && rectTransform.position.x > expandMin)
+        {
+            rectTransform.localScale = new Vector3(0.375f, 1.5f, 1.5f);
+        }
+        else
+        {
+            rectTransform.localScale = new Vector3(0.25f, 1f, 1f);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
