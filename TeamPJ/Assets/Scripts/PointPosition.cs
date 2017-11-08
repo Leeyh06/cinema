b407969c0a8ea54606324b38396edaa2546f53b6
SANGHALEE <sangha206@naver.com>
2017-11-08 12:43:32 +0900

    좌석선택 씬
    좌석별 이미지를 개별구현하기보단 마우스(VR 포인터) 위치좌표를 계산해서 좌석 선택하는게 효율적일 것 같아서 그렇게 함

---
+++ TeamPJ/Assets/Scripts/PointPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointPosition : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log(Input.mousePosition); //왼하가 0,0
+        }
+    }
+}
