ee6c348c750c5d6d8e458b6888b367537fbccb50
SANGHALEE <sangha206@naver.com>
2017-11-08 17:06:14 +0900

    AR
    숫자올라가는거

---
+++ AR/Assets/Scripts/BallControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour
+{
+    public Text score;
+    private int scoreText = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        scoreText += 1;
+        score.text = "" + scoreText;
+    }
+}
