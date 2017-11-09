4562818bd3bc3c60cac3a39a73bc0a1cd0a75285
SANGHALEE <sangha206@naver.com>
2017-11-09 17:09:33 +0900

    시간표
     시간표 화면

---
+++ TeamPJ/Assets/Scripts/MovieNameStorage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieNameStorage : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
