bb921095a55f4cb2f786b6c351d6eb398f383343
HovvoH <33311298+HovvoH@users.noreply.github.com>
2017-11-09 12:14:05 +0900

    좌석 이동 구현
    방향키 이동
    Player Box 가 카메라.

---
+++ TeamPJ/Assets/Slider Menu Free_New Edition/Scripts/CameraContor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContor : MonoBehaviour {
+
+    public GameObject PlayerBox; // 플레이어 오브젝트
+    public float Mun;
+
+    Vector3 PlayerPos; // 플레이어 포지션
+
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && !Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveCameraleft(PlayerBox);
+        }
+
+        if (!Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveCameraRight(PlayerBox);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveCameraForward(PlayerBox);
+        }
+
+        if (!Input.GetKeyDown(KeyCode.UpArrow) && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            MoveCameraBack(PlayerBox);
+        }
+
+    }
+
+    void MoveCameraleft(GameObject CameraBox)
+    {
+        CameraBox.transform.position += Vector3.left * Mun * Time.deltaTime;
+    }
+
+    void MoveCameraRight(GameObject CameraBox)
+    {
+        CameraBox.transform.position += Vector3.right * Mun * Time.deltaTime;
+    }
+
+    void MoveCameraForward(GameObject CameraBox)
+    {
+        CameraBox.transform.position += Vector3.forward * Mun * Time.deltaTime;
+    }
+
+    void MoveCameraBack(GameObject CameraBox)
+    {
+        CameraBox.transform.position += Vector3.back * Mun * Time.deltaTime;
+    }
+
+
+
+}
