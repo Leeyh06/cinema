4ebad90dd98054d8efc04d6b81fcea3d5633c10a
Lee <leeyh06@naver.com>
2017-11-10 13:49:12 +0900

    Merge branch 'MovieChoiceSlider' of https://github.com/Leeyh06/cinema into MovieChoiceSlider

---
+++ TeamPJ/Assets/Scripts/ButtonClicked.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonClicked : MonoBehaviour
+{
+    public static string textdata;
+    private bool checkedtest = false;
+
+    public void Onclicked()
+    {
+        textdata = GetComponentInParent<Text>().text + GetComponentInChildren<Text>().text;
+
+        if (checkedtest)
+        {//버튼선택된 상태에서 한번더 클릭하면 활성화
+            GetComponentInChildren<Text>().color = Color.black;
+            checkedtest = false;
+        }
+        else
+        {//자리버튼 선택시 활성화
+            checkedtest = true;
+
+            //GetComponent<Button>()
+            //textdata
+
+            GetComponentInChildren<Text>().color = Color.white;
+        }
+
+        Debug.Log(textdata);
+        if (SceneManager.GetActiveScene().name.Equals("SelectSeat"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
