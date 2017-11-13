b4b85023c58f90b4b09239162b6682391216991d
SANGHALEE <sangha206@naver.com>
2017-11-13 10:19:59 +0900

    1113
    1113

---
+++ TeamPJ/Assets/Scripts/StorageReset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StorageReset : MonoBehaviour
+{
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().isLoaded)
+        {
+            if (PlayerPrefs.HasKey("MovieName"))
+            {
+                PlayerPrefs.SetString("MovieName", null);
+            }
+        }
+    }
+}
